395295ac06ad349d4b6c73972e7f36db261fc4b4
Dylan T. Moberg <dylanmoberg@msn.com>
2015-10-02 14:14:20 -0700

    added css and map

--- server.js
+++ server.js
@@ -1,4 +1,12 @@
 var express = require('express');
-
+var bodyParser = require('body-parser');
 var app = express();
 
+var PORT = process.env.PORT || 8000;
+
+var request = require('superagent');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static(__dirname));
